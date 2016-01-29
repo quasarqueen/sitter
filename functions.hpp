e2ed8528ffb55e7dbb31a093ba9a412e198bf4fb
quasarqueen <bbladorn@yahoo.com>
2016-01-29 14:59:44 -0500

    Update functions.hpp
    can handle start between 5 and arbitrarily decided bedtime of 9, and any end time

--- functions.hpp
+++ functions.hpp
@@ -1,4 +1,16 @@
 int total_owed (int st, int end)
 {
-    return (end-st)*12;
+    if (end >=5 && end <=9){
+        return (end-st)*12;
+    }
+    else if (end >=9 && end <=12){
+        return (9-st)*12+(end-9)*8;
+    }
+    else if (end >=0 && end <=4){
+        return (9-st)*12+(3)*8+(end)*16;
+    }
+    else
+    {
+        return 0;
+    }
 }

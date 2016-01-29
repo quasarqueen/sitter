5e41461e98b171ffedd534e324d5ce78b33fea0d
quasarqueen <bbladorn@yahoo.com>
2016-01-29 15:11:50 -0500

    Update functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -1,13 +1,26 @@
 int total_owed (int st, int end)
 {
-    if (end >=5 && end <=9){
+    //start from 5-9
+    if (st>=5 && st <=9 && end >=5 && end <=9){
         return (end-st)*12;
     }
-    else if (end >=9 && end <=12){
+    else if (st>=5 && st <=9 && end >=9 && end <=12){
         return (9-st)*12+(end-9)*8;
     }
-    else if (end >=0 && end <=4){
+    else if (st>=5 && st <=9 && end >=0 && end <=4){
         return (9-st)*12+(3)*8+(end)*16;
+    }
+    //start from 9-12
+    else if (st>=9 && st <=12 && end >=9 && end <=12){
+        return (end-st)*8;
+    }
+    else if (st>=9 && st <=12 && end >=0 && end <=4){
+        return (12-st)*8+(end)*16;
+    }
+    //start from 12-4
+
+    else if (st>=0 && st <=4 && end >=0 && end <=4){
+        return (end-st)*16;
     }
     else
     {

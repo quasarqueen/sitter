157cd8182099fad7fa28df31f115a641e5d17dc4
quasarqueen <bbladorn@yahoo.com>
2016-01-29 15:17:45 -0500

    Update functions.hpp
    replace placeholder of 9 pm with variable 'bed'

--- functions.hpp
+++ functions.hpp
@@ -1,20 +1,20 @@
-int total_owed (int st, int end)
+int total_owed (int st, int end, int bed)
 {
     //start from 5-9
-    if (st>=5 && st <=9 && end >=5 && end <=9){
+    if (st>=5 && st <=bed && end >=5 && end <=bed){
         return (end-st)*12;
     }
-    else if (st>=5 && st <=9 && end >=9 && end <=12){
-        return (9-st)*12+(end-9)*8;
+    else if (st>=5 && st <=bed && end >=bed && end <=12){
+        return (bed-st)*12+(end-bed)*8;
     }
-    else if (st>=5 && st <=9 && end >=0 && end <=4){
-        return (9-st)*12+(3)*8+(end)*16;
+    else if (st>=5 && st <=bed && end >=0 && end <=4){
+        return (bed-st)*12+(12-bed)*8+(end)*16;
     }
-    //start from 9-12
-    else if (st>=9 && st <=12 && end >=9 && end <=12){
+    //start from bed-12
+    else if (st>=bed && st <=12 && end >=bed && end <=12){
         return (end-st)*8;
     }
-    else if (st>=9 && st <=12 && end >=0 && end <=4){
+    else if (st>=bed && st <=12 && end >=0 && end <=4){
         return (12-st)*8+(end)*16;
     }
     //start from 12-4

638ad75577e4607ace37acf3ec1d9dde0f1ef041
quasarqueen <bbladorn@yahoo.com>
2016-01-29 15:02:10 -0500

    Update tests.cpp
    more tests

--- tests.cpp
+++ tests.cpp
@@ -6,10 +6,14 @@ TEST (Pay, Total_owed)
     // equal hours worked to proper pay
     EXPECT_EQ(total_owed(5,5), 0);
     EXPECT_EQ(total_owed(5,6), 12);
-    //add tests for different time brackets
+    //add tests for different endtime brackets
     EXPECT_EQ(total_owed(6,11), 52);
     EXPECT_EQ(total_owed(6,2), 92);
     EXPECT_EQ(total_owed(5,4), 136);
+    //add tests for different start and end brackets
+    EXPECT_EQ(total_owed(9,11), 16);
+    EXPECT_EQ(total_owed(12,3), 48);
+    EXPECT_EQ(total_owed(10,4), 80);
 
 }
 
